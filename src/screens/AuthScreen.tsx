19c0ed1f5668ece968a20a788e62f5809be8d918
Ijlal Ahmad <ijlalahmad845@gmail.com>
2024-12-17 13:16:32 +0530

    auth screen improvements, just redirect to home when username found, rest can be managed from there

--- src/screens/AuthScreen.tsx
+++ src/screens/AuthScreen.tsx
@@ -1,39 +1,24 @@
 import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import LottieLoading from '../components/LottieLoading.tsx'
 import Grid from '@mui/material/Grid2'
 import ButtonsFragment from '../fragments/auth/ButtonsFragment.tsx'
-import { useSnackbar } from 'notistack'
-
-import { useAuthUser } from '../contexts/UserContext.tsx'
 import Canvas from '../fragments/auth/Canvas.tsx'
+import useAuthStore from '../store/auth.store.ts'
+import { GlobalConstants } from '../constants/GlobalConstants.ts'
+import { useNavigate } from 'react-router-dom'
+import useLocalStorage from '../hooks/useLocalStorage.ts'
 
 const AuthScreen: React.FC = () => {
     const navigate = useNavigate()
-    const { enqueueSnackbar } = useSnackbar()
-    const { userData, googleLogin, githubLogin, isLoading } = useAuthUser()
+    const { googleLogin, githubLogin } = useAuthStore()
+
+    const [username] = useLocalStorage(GlobalConstants.USERNAME, null)
 
     useEffect(() => {
-        if (isLoading) {
-            return
-        }
-        if (userData) {
+        if (username) {
             navigate('/')
-            enqueueSnackbar('Login Successfully', {
-                variant: 'success',
-                autoHideDuration: 3000,
-                anchorOrigin: {
-                    vertical: 'bottom',
-                    horizontal: 'right',
-                },
-                preventDuplicate: true,
-            })
         }
-    }, [isLoading, userData, enqueueSnackbar, navigate])
+    }, [navigate, username])
 
-    if (isLoading) {
-        return <LottieLoading />
-    }
     return (
         <Grid container sx={{ position: 'relative', width: '100vw', height: '100vh' }}>
             <Grid sx={{ width: '100%', height: '100%' }} className={'hidden lg:flex'}>
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import ChatsFragment from '../fragments/home/ChatsFragment.tsx'
-import useLocalStorage from '../hooks/useLocalStorage.ts'
-import { GlobalConstants } from '../constants/GlobalConstants.ts'
-// import EmptyChatFragment from '../fragments/home/EmptyChatFragment.tsx'
 import ChattingFragment from '../fragments/home/ChattingFragment.tsx'
 import UserProfileFragment from '../fragments/home/UserProfileFragment.tsx'
 
 const HomeScreen: React.FC = () => {
-    const [username] = useLocalStorage(GlobalConstants.USERNAME, '')
-    console.log(username)
     const [profileOpen, setProfileOpen] = React.useState<boolean>(false)
     return (
         <div className={'flex flex-row bg-slate-900/90 '}>
