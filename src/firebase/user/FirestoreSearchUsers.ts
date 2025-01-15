e001a3e215ae00d7f3dee39d4dbb6bde84e33200
GitHub Action <actions@github.com>
2025-01-15 12:46:46 +0000

    Formatted code in following files: \nsrc/firebase/user/FirestoreSearchUsers.ts

--- src/firebase/user/FirestoreSearchUsers.ts
+++ src/firebase/user/FirestoreSearchUsers.ts
@@ -1,39 +1,49 @@
-import { User } from "firebase/auth";
-import { Firestore, collection, endAt, getDocs, orderBy, query, startAt, where } from "firebase/firestore";
-import UserModel from "../../models/user.model";
-import { FirestoreCollections } from "../../constants/FireStoreCollections";
-import { UserConstants } from "../../constants/UserConstants";
+import { User } from 'firebase/auth'
+import {
+    Firestore,
+    collection,
+    endAt,
+    getDocs,
+    orderBy,
+    query,
+    startAt,
+    where,
+} from 'firebase/firestore'
+import UserModel from '../../models/user.model'
+import { FirestoreCollections } from '../../constants/FireStoreCollections'
+import { UserConstants } from '../../constants/UserConstants'
 
 class FirestoreSearchUsers {
     static searchUsers(
         firestore: Firestore,
         loggedInUser: User | null,
         SearchQuery: string,
-        SearchResult: (userList: UserModel[]) => void,
+        SearchResult: (userList: UserModel[]) => void
     ) {
-        
-        const users = collection(firestore, FirestoreCollections.USERS_COLLECTION);
+        const users = collection(firestore, FirestoreCollections.USERS_COLLECTION)
 
-        const userQuery = query(users,
-            where(UserConstants.USERNAME, "!=", ""),
+        const userQuery = query(
+            users,
+            where(UserConstants.USERNAME, '!=', ''),
             orderBy(UserConstants.USERNAME),
             startAt(SearchQuery),
-            endAt(SearchQuery + "\uf8ff"))
-            getDocs(userQuery)
+            endAt(SearchQuery + '\uf8ff')
+        )
+        getDocs(userQuery)
             .then((userQuerySnapshot) => {
-                const userList: UserModel[] = [];
+                const userList: UserModel[] = []
                 userQuerySnapshot.forEach((doc) => {
-                    const user = doc.data() as UserModel;
+                    const user = doc.data() as UserModel
                     if (user.uid !== loggedInUser?.uid) {
-                        userList.push(user);
+                        userList.push(user)
                     }
-                });
-                SearchResult(userList);
+                })
+                SearchResult(userList)
             })
             .catch((error) => {
-                console.error("Error searching users:", error);
-                SearchResult([]);
-            });
-        }
+                console.error('Error searching users:', error)
+                SearchResult([])
+            })
+    }
 }
 export default FirestoreSearchUsers
