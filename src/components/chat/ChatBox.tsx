222cc7e4c0da53dc024afcfecb41bd2dabc30274
SHADABALIGITHUB <shad46243@gmail.com>
2025-03-31 04:23:25 +0530

    bottom scroll bottom issue

--- src/components/chat/ChatBox.tsx
+++ src/components/chat/ChatBox.tsx
@@ -1,4 +1,4 @@
-import React,{useRef} from 'react'
+import React, { useRef,useState,useEffect } from 'react'
 import { ChatMessageType } from '../../enums/ChatMessageType.ts'
 import useChatDetailsStore from '../../store/chat.details.store.ts'
 import useLocalStore from '../../store/local.store.ts'
@@ -12,47 +12,55 @@ import EmptyChatFragment from '../../fragments/home/EmptyChatFragment.tsx'
 import ItemChatHelloMessage from '../listItems/ItemChatHelloMessage.tsx'
 import ChatMessageModel from '../../models/chat.message.model.ts'
 import { Button } from '@mui/material'
-// import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 
 const ChatBox: React.FC = () => {
     const currentChat = useChatDetailsStore((state) => state.chatDetails)
     const username = useLocalStore((state) => state.username)
-    const chatContainerRef = useRef<HTMLDivElement>(null);
-    // const [showScrollButton, setShowScrollButton] = useState(false);
+    const chatContainerRef = useRef<HTMLDivElement>(null)
+    const [showScrollButton, setShowScrollButton] = useState(false);
 
     // Scroll to bottom function
-    // const scrollToBottom = () => {
-    //     if (chatContainerRef.current) {
-    //         chatContainerRef.current.scrollTo({
-    //             top: chatContainerRef.current.scrollHeight,
-    //             behavior: 'smooth',
-    //         });
-    //     }
-    // };
+    const scrollToBottom = () => {
+        if (chatContainerRef.current) {
+            chatContainerRef.current.scrollTo({
+                top: chatContainerRef.current.scrollHeight,
+                behavior: 'smooth',
+            });
+            setShowScrollButton(false)
+        }
+    };
+    const debounce = <T extends (...args: any[]) => void>(
+        func: T,
+        delay: number
+    ): ((...args: Parameters<T>) => void) => {
+        let timeoutId: ReturnType<typeof setTimeout> | undefined;
+
+        return (...args: Parameters<T>) => {
+            if (timeoutId) {
+                clearTimeout(timeoutId);
+            }
+            timeoutId = setTimeout(() => func(...args), delay);
+        };
+    };
 
     // Track scrolling
-    // useEffect(() => {
-    //     const handleScroll = () => {
-    //         if (chatContainerRef.current) {
-    //             const { scrollTop, scrollHeight, clientHeight } = chatContainerRef.current;
-    //             setShowScrollButton(scrollHeight - (scrollTop + clientHeight) > 100);
-    //         }
-    //     };
-    //
-    //     const chatBox = chatContainerRef.current;
-    //     if (chatBox) {
-    //         chatBox.addEventListener('scroll', handleScroll);
-    //     }
-    //
-    //     return () => {
-    //         if (chatBox) {
-    //             chatBox.removeEventListener('scroll', handleScroll);
-    //         }
-    //     };
-    // }, []);
+    useEffect(() => {
+        const chatContainer = chatContainerRef.current;
+        if (!chatContainer) return;
 
+        const handleScroll = debounce(() => {
+            setShowScrollButton(chatContainer.scrollTop + chatContainer.clientHeight < chatContainer.scrollHeight - 50);
+        }, 100);
+
+        chatContainer.addEventListener('scroll', handleScroll);
+
+        return () => {
+            chatContainer.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     if (currentChat === null) {
-        return <EmptyChatFragment />;
+        return <EmptyChatFragment />
     }
     const TextMessage = ({ message }: { message: ChatMessageModel }) => {
         if (message.from !== username) {
@@ -145,37 +153,38 @@ const ChatBox: React.FC = () => {
     }
 
     return (
-        <div
-            className={
-                'z-0 flex-1 bg-white overflow-y-scroll flex flex-col-reverse ' +
-                'scrollbar-thin scrollbar-thumb-slate-500/50 scrollbar-track-white scrollbar-thumb-rounded-full'
-            } ref={chatContainerRef}>
-            {currentChat.chatMessages.map((message) => (
-                <div className={'flex gap-2'} key={message.id}>
-                    {/*First Message*/}
-                    {message.type === ChatMessageType.TypeFirstMessage && <ItemChatHelloMessage />}
-                    {/*text Message*/}
-                    {message.type === ChatMessageType.TypeText && <TextMessage message={message} />}
-                    {/*Image Message*/}
-                    {message.type === ChatMessageType.TypeImage && (
-                        <ImageMessage message={message} />
-                    )}
-                    {/*Sticker Message*/}
-                    {message.type === ChatMessageType.TypeSticker && (
-                        <StickerMessage message={message} />
-                    )}
-                </div>
-            ))}
-            {/*{showScrollButton && (*/}
-                <Button
-                    // onClick={scrollToBottom}
-                    className="fixed top-full right-0 bg-blue-500 text-white px-4 py-2"
-                >
-                    {/*<KeyboardDoubleArrowDownIcon/>*/}
-                    How can We Win The Show
+            <div
+                className={
+                    'z-0 flex-1 bg-white overflow-y-scroll flex flex-col-reverse relative' +
+                    'scrollbar-thin scrollbar-thumb-slate-500/50 scrollbar-track-white scrollbar-thumb-rounded-full'
+                }
+                ref={chatContainerRef}>
+                {currentChat.chatMessages.map((message) => (
+                    <div className={'flex gap-2'} key={message.id}>
+                        {/*First Message*/}
+                        {message.type === ChatMessageType.TypeFirstMessage && (
+                            <ItemChatHelloMessage />
+                        )}
+                        {/*text Message*/}
+                        {message.type === ChatMessageType.TypeText && (
+                            <TextMessage message={message} />
+                        )}
+                        {/*Image Message*/}
+                        {message.type === ChatMessageType.TypeImage && (
+                            <ImageMessage message={message} />
+                        )}
+                        {/*Sticker Message*/}
+                        {message.type === ChatMessageType.TypeSticker && (
+                            <StickerMessage message={message} />
+                        )}
+                    </div>
+                ))}
+                {showScrollButton &&
+                <Button onClick={scrollToBottom} sx={{position:"absolute",right:'50px',bottom:"100px",backgroundColor:"skyblue",borderRadius:'100%',color:'white'}} className="w-12 h-12">
+                    <KeyboardDoubleArrowDownIcon/>
                 </Button>
-            {/*)}*/}
-        </div>
+                }
+            </div>
     )
 }
 
